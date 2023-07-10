e36537ccebccb91bf59ab19638bad02a51fe4e34
Ethan-LN <et.l.nicholas@gmail.com>
2023-07-10 21:11:41 +1000

    implemented the Radar chart

---
+++ src/components/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import RadarCharts from './RadarCharts'
+
+export const Dashboard = () => {
+  return (
+    <div>
+      Dashboard
+      <RadarCharts />
+    </div>
+  )
+}
